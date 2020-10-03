82bd28e5ab10b47eae8bdf9b1fd74b0c3e331743
Elharam4 <55320959+elostora@users.noreply.github.com>
2020-10-03 11:31:02 +0200

    Create elharam4.sh

---
+++ elharam4.sh
@@ -0,0 +1,14 @@
+
+clear
+echo "__________"
+echo "< elharam4 >"
+echo "----------"
+echo "    \    "
+echo "     \    "
+echo "       coded by : Elharam4               "
+echo
+read -p "Enter The domain: " df
+echo
+lynx -listonly -dump $df
+echo
+echo "Elharam4"
